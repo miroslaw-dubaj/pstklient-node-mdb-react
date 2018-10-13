61f9243aad4810ba291de6802dc451729b4c4c8b
Miroslaw Dubaj <miroslaw.dubaj@gmail.com>
2018-10-13 20:31:35 +0200

    api test and docker nodemon container in progress

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
+mongoose
+  .connect(
+    `mongodb://${process.env.ME_CONFIG_MONGODB_ADMINUSERNAME}:${
+      process.env.ME_CONFIG_MONGODB_ADMINPASSWORD
+    }@mongo:27017`
+  )
+  .then(() => console.log('MongoDB database connected to server'))
+  .catch(err => console.log(err));
+
 app.get('/', (req, res) => res.send('Hello world'));
 
-const port = process.env.PORT || 8080;
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+module.exports = app;
