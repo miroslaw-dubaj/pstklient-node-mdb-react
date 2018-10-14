2fb83238a0322a0ad41727f9e5f3165acc0c2c60
Miroslaw Dubaj <miroslaw.dubaj@gmail.com>
2018-10-14 11:37:52 +0200

    user registration basic implementatin

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -7,6 +8,11 @@ const posts = require('./routes/api/posts');
 
 const app = express();
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Connect to MongoDB
 mongoose
   .connect(
     `mongodb://${process.env.ME_CONFIG_MONGODB_ADMINUSERNAME}:${
@@ -16,8 +22,10 @@ mongoose
   .then(() => console.log('MongoDB database connected to server'))
   .catch(err => console.log(err));
 
+// Test route
 app.get('/', (req, res) => res.send('Hello node'));
 
+// Api routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
